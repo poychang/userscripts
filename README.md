f7c361c77b1de69ec127dc79930b316cd6517dc1
Poy Chang <poypost@gmail.com>
2024-10-19 22:56:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# UserScripts
+
+透過 [Tampermonkey](https://www.tampermonkey.net/)、[Userscripts](https://github.com/quoid/userscripts) 所支援的使用者腳本 (Userscript) 
+改善瀏覽器的使用體驗。
+
+
+## 相關連結
+
+- [Tampermonkey Documentation](https://www.tampermonkey.net/documentation.php)
+    - [@run-at](https://www.tampermonkey.net/documentation.php#_run_at)
+- [Userscripts](https://github.com/quoid/userscripts)
---
+++ src/!demo-user-script.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Demo user script
+// @description  I am a demo user script that you can safely delete (add any files to this folder and I will no longer automatically generate)
+// @author       Userscripts
+// @version      0.0.1
+// @updateURL    https://raw.githubusercontent.com/poychang/userscripts/main/src/!demo-user-script.user.js
+// @match        *://*/*
+// @grant        none
+// @inject-into  content
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // here is your code
+})();
